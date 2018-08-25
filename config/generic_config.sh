ecaa3ef4c9748f5318d9ce1b0ebf4bae0d44b4a3
Marc Oggier <marc.oggier@megavolts.ch>
2018-08-25 13:10:16 -0800

    Update generic_config.sh

--- config/generic_config.sh
+++ config/generic_config.sh
@@ -1,5 +1,6 @@
 #/bin/bash!
 PASSWORD=$1
+
 echo -e "Entering chroot"
 echo -e "Tuning pacman"
 
@@ -15,7 +16,7 @@ pacman -Syu
 
 echo -e ".. change locales"
 echo "FONT=lat9w-16" >> /etc/vconsole.conf
-wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/X220/source/locale.gen -O /etc/locale.gen
+wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/source/locale.gen -O /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 locale-gen
 export LANG=en_US.UTF-8
@@ -25,37 +26,11 @@ rm /etc/localtime
 ln -sf /usr/share/zoneinfo/America/Anchorage /etc/localtime
 hwclock --systohc --utc
 
-echo -e ".. set hostname to adak"
-echo adak > /etc/hostname
-
-echo -e ".. generating initramfs"
-wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/X220/source/mkinitcpio.conf -O /etc/mkinitcpio.conf
-mkinitcpio -p linux-zen
-
-echo -e ".. creating service file for initramfs regeneration"
-wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/X220/source/initramfs-update.path -O /etc/systemd/system/initramfs-update.path
-wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/X220/source/initramfs-update.service -O /etc/systemd/system/initramfs-update.service
-systemctl enable initramfs-update.path
-
 echo -e ".. setting root password"
 passwd root << EOF
 $PASSWORD
 $PASSWORD
 EOF
-
-echo -e ".. install bootload:"
-# copy vmlinuz and initramfs
-mkdir /boot/EFI/zen
-cp /boot/vmlinuz-linux-zen /boot/EFI/zen/vmlinuz-zen.efi
-cp /boot/initramfs-linux-zen.img /boot/EFI/zen/archlinux-zen.img
-# *wget /backup/refindo.conf -O /boot/EFI/refind/refind.conf*
-# change /dev/sdb4 with uuid
-
-pacman -Sy refind-efi --noconfirm
-refind-install
-# create a cryptab entry
-mv /home.keyfile /etc/home.keyfile
-echo "home /dev/sdb5 /etc/home.keyfile" >> /etc/crypttab
 
 echo -e ".. create user megavolts with default password"
 useradd -m -g users -G wheel,audio,disk,lp,network -s /bin/bash megavolts
@@ -68,7 +43,6 @@ echo -e " ... adding megavolts to wheel"
 sed 's /# %wheel ALL=(ALL) ALL/%  wheel ALL=(ALL) ALL/' /etc/sudoers
 
 systemctl enable sshd
-
 pacman -S mlocate rsync --noconfirm
 updatedb
 
@@ -97,7 +71,6 @@ yaourtpkg() {
 
 yaourtpkg -grml-zsh-config
 chsh -s $(which zsh)
-chsh -s $(which zsh) megavolts
 
 echo -e ".. Configure pacman"
 yaourtpkg reflector
@@ -106,20 +79,13 @@ reflector --latest 200 --protocol http --protocol https --sort rate --save /etc/
 wget https://raw.githubusercontent.com/megavolts/ArchLinux/X220/master/source/mirrorupgrade.hook -P /etc/pacman.d/hooks/
 
 echo -e ".. Install xorg and input"
-pacman -S --noconfirm xorg-server xorg-apps xf86-video-intel mesa-libgl lib32-mesa-libgl libva-intel-driver libva xorg-xinit xorg-xrandr <<EOF
+pacman -S --noconfirm xorg-server xorg-apps xorg-xinit xorg-xrandr <<EOF
 all
 1
 EOF
-pacman -S --noconfirm xf86-input-synaptics xf86-input-keyboard xf86-input-wacom xf86-input-mouse
-
-echo -e "... rebuilding initramfs with i915"
-echo "options i915 enable_rc6=1 enable_fbc=1 lvds_downclock=1" >> /etc/modprobe.d/i915.conf
-mkinitcpio -p linux-zen
-cp /boot/vmlinuz-linux-zen /boot/EFI/zen/vmlinuz-zen.efi
-cp /boot/initramfs-linux-zen.img /boot/EFI/zen/archlinux-zen.img
 
 echo -e "... install plasma windows manager"
-pacman -S plasma-desktop sddm networkmanager powerdevil plasma-nm kscreen --noconfirm
+pacman -S plasma-desktop sddm networkmanager powerdevil plasma-nm kscreen plasma-pa pavucontrol--noconfirm
 
 echo -e "... configure sddm"
 sddm --example-config > /etc/sddm.conf
@@ -131,11 +97,8 @@ echo -e "... enable NetworkManager"
 systemctl enable NetworkManager.service
 systemctl start NetworkManager.service
 
-echo -e "... set hostname to adak"
-hostnamectl set-hostname adak
-
 echo -e ".. install audio server"
-yaourtpkg "alsa-utils pulseaudio pulseaudio-alsa pulseaudio-jack pulseaudio-equalizer kmix libcanberra-pulse libcanberra-gstreamer"
+yaourtpkg "alsa-utils pulseaudio pulseaudio-alsa pulseaudio-jack pulseaudio-equalizer kmix libcanberra-pulse libcanberra-gstreamer "
 
 wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/source/software_install.sh
 chmod +x software_install.sh
@@ -148,9 +111,6 @@ if id userbuild >/dev/null 2>&1; then
   sed -i 's/builduser ALL=(ALL) ALL//' /etc/sudoerselse
 fi
 
-echo ".. updating kernel image in /boot"
-cp /boot/vmlinuz-linux-zen /boot/EFI/zen/vmlinuz-zen.efi
-cp /boot/initramfs-linux-zen.img /boot/EFI/zen/archlinux-zen.img
-rm software_install.sh
-rm /config_archlinux.sh
-exit
+# remove config files
+# run specific_config.sh >> remove unnecessary graphic drivers
+# message: don't forget to change root passwords
