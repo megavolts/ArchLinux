23755b68c3340504e9d12fd30a0c0d2dbcb2c4bd
Marc Oggier <marc.oggier@megavolts.ch>
2018-08-25 13:06:29 -0800

    Update and rename x220_specific_config.sh to specific_config.sh

--- X220/source/specific_config.sh
+++ X220/source/specific_config.sh
@@ -4,6 +4,7 @@ root_dev=$1
 home_dev=$2
 
 echo -e ".. set hostname to adak"
+hostnamectl set-hostname adak
 echo adak > /etc/hostname
 
 echo -e ".. generating initramfs"
@@ -15,31 +16,36 @@ wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/X220/source/in
 wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/X220/source/initramfs-update.service -O /etc/systemd/system/initramfs-update.service
 systemctl enable initramfs-update.path
 
-echo -e ".. config bootloader"
-# copy vmlinuz and initramfs
+echo -e ".. install drivers specific to X220"
+pacman -S --noconfirm xf86-video-intel mesa-libgl lib32-mesa-libgl libva-intel-driver libva <<EOF
+all
+1
+EOF
+pacman -S --noconfirm xf86-input-synaptics xf86-input-keyboard xf86-input-wacom xf86-input-mouse
+
+echo -e "... rebuilding initramfs with i915"
+echo "options i915 enable_rc6=1 enable_fbc=1 lvds_downclock=1" >> /etc/modprobe.d/i915.conf
+mkinitcpio -p linux-zen
 mkdir /boot/EFI/zen
 cp /boot/vmlinuz-linux-zen /boot/EFI/zen/vmlinuz-zen.efi
 cp /boot/initramfs-linux-zen.img /boot/EFI/zen/archlinux-zen.img
 
-wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/X220/source/refind.conf -O /boot/EFI/refind/refind.conf
-sed -i "s|ROOT_UUID|$(blkid -o value -s UUID /dev/$root_dev)|" /boot/EFI/refind/refind.conf
-
+echo -e ".. install bootloader"
 pacman -Sy refind-efi --noconfirm
 refind-install
+# change root_dev for its uuid
+wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/X220/source/refind.conf -O /boot/EFI/refind/refind.conf
+sed -i "s|ROOT_UUID|$(blkid -o value -s UUID /dev/$root_dev)|" /boot/EFI/refind/refind.conf
 
 # create a cryptab entry
 mv /home.keyfile /etc/home.keyfile
 echo "home /dev/sdb5 /etc/home.keyfile" >> /etc/crypttab
+# for part uuid
+#home_uuid=$(blkid -o value -s UUID /dev/$home_dev)
+#echo "home UUID="$home_uuid" /etc/home.keyfile" 
 
 echo ".. updating kernel image in /boot"
 cp /boot/vmlinuz-linux-zen /boot/EFI/zen/vmlinuz-zen.efi
 cp /boot/initramfs-linux-zen.img /boot/EFI/zen/archlinux-zen.img
 rm software_install.sh
 rm /config_archlinux.sh
-exit
-
-
-## Function
-get_uuid() {
-    blkid -o export "$1" | grep UUID | awk -F= '{print $2}'
-}
