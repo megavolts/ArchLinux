59a7887dff8271cfcca7f81bbedf212bcf9971bc
Marc Oggier <marc.oggier@megavolts.ch>
2019-06-09 13:15:37 -0800

    Update and rename X220_archlinux_reinstall_script.sh to X220_archlinux_reinstall_script-btrfs.sh

--- X220/X220_archlinux_reinstall_script-btrfs.sh
+++ X220/X220_archlinux_reinstall_script-btrfs.sh
@@ -1,6 +1,14 @@
 #/bin/bash
 # Reinstall
 #
+
+#
+#  1            2048         1067007   512.0 MiB   EF00  EFI system partition
+#  2         1067008         1099775   16.0 MiB    0C01  Microsoft reserved ...
+#  3                                   16.0 GiB    8200  Linux SWAP on LVM
+#  4         1099776       261598247   124.2 GiB   0700  Basic data partition
+#  5       261599232       523743231   125.0 GiB   8300  Linux filesystem on LVM
+
 root_dev=sdb4
 home_dev=sdb6
 
