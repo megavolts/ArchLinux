d3af45cbbb99703c9cd20524fa132b90ca77f9bf
Marc Oggier <marc.oggier@megavolts.ch>
2019-08-29 09:30:28 -0800

    Update install_software.sh

--- config/install_software.sh
+++ config/install_software.sh
@@ -1,144 +1,97 @@
 #!/bin/bash
 # execute as root
-
-if [[ $( grep builduser /etc/passwd) ]]; then
-  # create a fake builduser
-  useradd builduser -m # Create the builduser
-  passwd -d builduser # Delete the buildusers password
-  echo "builduser ALL=(ALL) ALL" >> /etc/sudoers
-  USER_FLAG=0
-fi
-
-echo -e ".. Install xorg and input"
-pacman -S --noconfirm xorg-server xorg-apps xorg-xinit xorg-xrandr <<EOF
-all
-1
-EOF
-
-echo -e "... install plasma windows manager"
-pacman -S plasma-desktop sddm networkmanager powerdevil plasma-nm kscreen plasma-pa pavucontrol--noconfirm
-
-echo -e "... configure sddm"
-pacman -S sddm --noconfirm
-sddm --example-config > /etc/sddm.conf
-sed -i 's/Current=/Current=breeze/' /etc/sddm.conf
-sed -i 's/CursorTheme=/CursorTheme=breeze_cursors/' /etc/sddm.conf
-systemctl enable sddm
-
-# Function
-buildpkg(){
-  CURRENT_DIR=$pwd
-  wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
-  tar -xvzf $1.tar.gz -C /home/builduser
-  chown builduser:builduser /home/builduser/$1 -R
-  cd /home/builduser/$1
-  sudo -u builduser bash -c "makepkg -si --noconfirm"
-  cd $CURRENT_dir 
-  rm /home/builduser/$1 -R
-  rm $1.tar.gz
-}
+# 2019-08-29
+USER=$1
+echo $USER
+# sublime-text-dev
 
 yaourtpkg() {
-  sudo -u builduser bash -c "yaourt -S --noconfirm $1"
+  sudo -u $USER bash -c "yaourt -Syu --noconfirm $1"
 }
+
+packages=''
 
 echo -e "Install software"
 echo -e ".. basic tools"
-yaourtpkg "yakuake tmux kdialog kfind xorg-xkill arp-scan"
+yaourtpkg 'yakuake tmux kdialog kfind arp-scan'
 
 echo -e ".. partition tools"
-yaourtpkg "gparted ntfs-3g exfat-utils mtools"
+yaourtpkg 'gparted ntfs-3g exfat-utils mtools'
 
-echo -e "... installing fonts"
-yaourtpkg "ttf-dejavu font-mathematica ttf-mathtype ttf-vista-fonts ttf-freefont ttf-inconsolata ttf-hack ttf-anonymous-pro ttf-freefont ttf-liberation"
+# echo -e "... installing fonts"
+yaourtpkg 'ttf-dejavu font-mathematica ttf-mathtype freefonts ttf-inconsolata ttf-hack ttf-anonymous-pro ttf-liberation'
 
-echo -e ".. internet software"
-yaourtpkg "firefox thunderbird filezilla profile-sync-daemon"
-yaourtpkg "nextcloud-client qownnotes"
 
-echo -e "... configure profile-sync-daemon to improve speed, reduced wear to physical drives"
-echo "megavolts ALL=(ALL) NOPASSWD: /usr/bin/psd-overlay-helper" >> /etc/sudoers
-wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/config/psd.conf -O /home/megavolts/.config/psd/psd.conf
-systemctl --user start psd
+# echo -e ".. internet software"
+yaourtpkg 'firefox thunderbird filezilla  nextcloud-client'
 
-echo -e ".. coding tools"
-yaourtpkg "sublime-text-dev pycharm-community-edition python-pip"
+systemctl --user enable psd
 
-echo -e ".. media"
-yaourtpkg "dolphin dolphin-plugins qt5-imageformats ffmpegthumbs"
-yaourtpkg "ark unrar p7zip unzip"
+# echo -e ".. coding tools"
+yaourtpkg 'sublime-text-dev'
 
-echo -e "... viewer"
-yaourtpkg "okular spectacle discount kdegraphics-mobipocket"
+# echo -e ".. media"
+yaourtpkg 'dolphin dolphin-plugins qt5-imageformats ffmpegthumbs lzop kdegraphics-thumbnailers'
+yaourtpkg 'openconnect networkmanager-openconnect'
 
-echo -e "... images"
-yaourtpkg "imagemagick guetzli geeqie inkscape gimp darktable libraw hugin-hg"
+yaourtpkg 'ark unrar p7zip unzip'
 
-echo -e "... musics and videos"
-yaourtpkg "vlc amarok plex-media-server-plexpass plex-media-player ffmpeg"
+# echo -e "... viewer"
+yaourtpkg 'okular spectacle discount kdegraphics-mobipocket'
+
+# echo -e "... images"
+yaourtpkg 'imagemagick guetzli geeqie inkscape gimp darktable libraw hugin-hg'
+
+# echo -e "... musics and videos"
+yaourtpkg 'vlc plex-media-server-plexpass plex-media-player ffmpeg'
+
+# echo -e ".. office"
+yaourtpkg 'libreoffice-fresh mendeleydesktop texmaker texlive-most'
+yaourtpkg 'aspell-fr aspell-en aspell-de hunspell-en_US hunspell-fr hunspell-de hyphen-en hyphen-en hyphen-de libmythes mythes-en mythes-fr libreoffice-extension-grammalecte-fr'
+
+# echo -e ".. printing tools"
+yaourtpkg 'cups system-config-printer'
+
+yaourtpkg 'virtualbox virtualbox-guest-iso virtualbox-host-dkms linux-zen-headers virtualbox-ext-oracle linux-zen-headers'
+
+
+# # internet messenging
+yaourtpkg 'telegram-desktop'
+
+# # python packages
+yaourtpkg 'pycharm-community-edition python-pip'
+yaourtpkg 'python-numpy python-matplotlib python-scipy python-pandas python-openpyxl python-basemap'
+yaourtpkg 'python-numpy python-matplotlib python-scipy python-pandas python-openpyxl ipython jupyter cython'
+
+# # citation
+yaourtpkg 'mendeleydesktop'
+
+# yaourtpkg 'packages
+yaourtpkg packages
+echo -e "... don't forget to install Antidote"
+
+# # # Plex Media Player
+# # yaourtpkg '"synergy "
 systemctl start plexmediaserver
 systemctl enable plexmediaserver
+echo -e "... to configure plex-media-server visit http://localhost:32400/web/"
 
-echo -e ".. office"
-yaourtpkg "libreoffice-fresh mendeleydesktop texmake texlive-most"
-yaourtpkg "aspell-fr aspell-en aspell-de hunspell-en hunspell-fr hunspell-de hyphen-en hyphen-en hyphen-de libmythes mythes-en mythes-fr libreoffice-extension-grammalecte-fr"
-
-echo -e ".. printing tools"
-yaourtpkg "cups foomatic-db foomatic-db-engine foomatic-db-nonfree"
-
-yaourtpkg "synergy"
-
-yaourtpkg "virtualbox virtualbox-guest-iso virtualbox-host-dkms linux-zen-headers virtualbox-ext-oracle"
+groupadd vboxusers
 gpasswd -a megavolts vboxusers
 echo "vboxdrv vboxnetadp vboxnetflt" >> /usr/lib/modules-load.d/virtualbox-host-dkms.conf    
 
-echo -e "... to configure plex-media-server visit http://localhost:32400/web/"
-echo -e "... don't forget to install Antidote"
-
-packages = ''
-
-# internet messenging
-packages +=  'telegram-desktop'
-
-# python packages
-packages=''
-packages+='python-numpy python-matplotlib python-scipy python-pandas openpyxml'
-yaourtpkg packages
-
-# logitech unifying
-packages+='solaar'
-
-packages+='flatpak'
-
-# citation
-packages+='mendeleydesktop'
-
-# wireless connection with phone
-packages+='kdeconnect sshfs'
-
-# bluetooth
-packages+='bluez bluez-utils bluedevil  pulseaudio-bluetooth '
-
-# add the following line to /etc/bluetooth/audio.conf to allow laptop speaker as a sink
-# ```
-# [General] 
-# Enable=Source
-# add the following lien to /etc/pulse/default.pa to auto connect to bluetooth
-# #automatically switch to newly-connected devices
-# load-module module-switch-on-connect
-#
 
 
+yaourtpkg 'snapper-gui-git'
 
-# Plex Media Player
-flatpak remote-add tingping https://dl.tingping.se/flatpak/tingping.flatpakrepo
-flatpak install tingping tv.plex.PlexMediaPlayer
+yaourtpkg 'xdg-desktop-portal xdg-desktop-portal-kde'
 
 
-if [[ USER_FLAG==0 ]]; then
-  userdel builduser
-  rm /home/builduser -R
-  sed -i 's/builduser ALL=(ALL) ALL//' /etc/sudoers
-fi
+yaourtpkg 'qownnotes profile-sync-daemon'
+echo -e "... configure profile-sync-daemon to improve speed, reduced wear to physical drives"
+echo "megavolts ALL=(ALL) NOPASSWD: /usr/bin/psd-overlay-helper" >> /etc/sudoers
+mkdir /home/megavolts/.config/psd/
+wget https://raw.githubusercontent.com/megavolts/ArchLinux/master/config/psd.conf -O /home/megavolts/.config/psd/psd.conf
+systemctl --user start psd
 
 exit
