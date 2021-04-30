f13a6f7a6c542f902d71da82f11d0be21a680856
Marc Oggier <marc.oggier@megavolts.ch>
2021-04-30 20:50:37 +0000

    Update 02-T14-generic_config.sh

--- T14/02-T14-generic_config.sh
+++ T14/02-T14-generic_config.sh
@@ -102,70 +102,70 @@ systemctl enable btrfs-scrub@-.timer
 
 
 # ## REMOVE UNDERNEATH MOVE TO FINALIZE AFTER REBOOT
-# # Enable snapshots with snapper
-# yaourtpkg snapper acl
+# # Enable snapshots with snapper 
+yaourtpkg snapper acl snapper-gui-git
 
-# echo -e "... >> Configure snapper"
-# snapper -c root create-config /
-# snapper -c home create-config /home
+echo -e "... >> Configure snapper"
+snapper -c root create-config /
+snapper -c home create-config /home
 
 # # we want the snaps located /at /mnt/btrfs-root/_snaptshot rather than at the root
-# btrfs subvolume delete /.snapshots
-# btrfs subvolume delete /home/.snapshots
+btrfs subvolume delete /.snapshots
+btrfs subvolume delete /home/.snapshots
 
-# btrfs subvolume create /mnt/btrfs-arch/@snapshots/@root_snaps
-# btrfs subvolume create /mnt/btrfs-arch/@snapshots/@home_snaps
+btrfs subvolume create /mnt/btrfs-arch/@snapshots/@root_snaps
+btrfs subvolume create /mnt/btrfs-arch/@snapshots/@home_snaps
 
 # # # mount subvolume to original snapper subvolume
-# mkdir /.snapshots
-# mkdir /home/.snapshots
-# mount -o compress=lzo,subvol=@snapshots/@root_snaps /dev/mapper/arch /.snapshots
-# mount -o compress=lzo,subvol=@snapshots/@home_snaps /dev/mapper/arch /home/.snapshots
+mkdir /.snapshots
+mkdir /home/.snapshots
+mount -o compress=lzo,subvol=@snapshots/@root_snaps /dev/mapper/arch /.snapshots
+mount -o compress=lzo,subvol=@snapshots/@home_snaps /dev/mapper/arch /home/.snapshots
 
 # echo "# Snapper subvolume" >> /etc/fstab # add snapper subvolume to fstab
-# echo "LABEL=arch /.snapshots btrfs rw,noatime,ssd,discard,compress=lzo,space_cache,subvol=@snapshots/@root_snaps   0 0" >> /etc/fstab
-# echo "LABEL=arch /home/.snapshots  btrfs rw,noatime,ssd,discardcompress=lzo,space_cache,subvol=@snapshots/@home_snaps   0 0" >> /etc/fstab
+echo "LABEL=arch /.snapshots btrfs rw,noatime,ssd,discard,compress=lzo,space_cache,subvol=@snapshots/@root_snaps   0 0" >> /etc/fstab
+echo "LABEL=arch /home/.snapshots  btrfs rw,noatime,ssd,discardcompress=lzo,space_cache,subvol=@snapshots/@home_snaps   0 0" >> /etc/fstab
 
-# sed -i "s/ALLOW_USERS=\"/ALLOW_USERS=\"$USER/g" /etc/snapper/configs/home # Allow $USER to modify the files
-# sed -i "s/ALLOW_USERS=\"/ALLOW_USERS=\"$USER/g" /etc/snapper/configs/root
-# sed 's/PRUNENAMES = "/PRUNENAMES = ".snapshots /g' -i /etc/updatedb.conf # do not index snapshot via mlocate
-# systemctl start snapper-timeline.timer snapper-cleanup.timer  # start and enable snapper
-# systemctl enable snapper-timeline.timer snapper-cleanup.timer
+sed -i "s/ALLOW_USERS=\"/ALLOW_USERS=\"$USER/g" /etc/snapper/configs/home # Allow $USER to modify the files
+sed -i "s/ALLOW_USERS=\"/ALLOW_USERS=\"$USER/g" /etc/snapper/configs/root
+sed 's/PRUNENAMES = "/PRUNENAMES = ".snapshots /g' -i /etc/updatedb.conf # do not index snapshot via mlocate
+systemctl start snapper-timeline.timer snapper-cleanup.timer  # start and enable snapper
+systemctl enable snapper-timeline.timer snapper-cleanup.timer
 
 # # change snapper timer to every 5 minutes
-# sed  "s|OnCalendar=hourly|OnCalendar=*:0\/5|g" -i /usr/lib/systemd/system/snapper-timeline.timer
-# sed  "s|OnUnitActiveSec=1d|OnUnitActiveSec=1h|g" -i /usr/lib/systemd/system/snapper-cleanup.timer
+sed  "s|OnCalendar=hourly|OnCalendar=*:0\/5|g" -i /usr/lib/systemd/system/snapper-timeline.timer
+sed  "s|OnUnitActiveSec=1d|OnUnitActiveSec=1h|g" -i /usr/lib/systemd/system/snapper-cleanup.timer
 
-# # change snap config for home directory
-# # sed  "s|TIMELINE_MIN_AGE=\"1800\"|TIMELINE_MIN_AGE=\"172800\"|g" -i /etc/snapper/configs/home      # keep all backup for 2 days (172800 seconds)
-# # sed  "s|TIMELINE_LIMIT_HOURLY=\"10\"|TIMELINE_LIMIT_HOURLY=\"96\"|g" -i /etc/snapper/configs/home  # keep daily backup fro 14 days (336 backup)
-# # sed  "s|TIMELINE_LIMIT_DAILY=\"10\"|TIMELINE_LIMIT_DAILY=\"14\"|g" -i /etc/snapper/configs/home    # keep daily backup for 14 days
-# # sed  "s|TIMELINE_LIMIT_WEEKLY=\"0\"|TIMELINE_LIMIT_WEEKLY=\"0\"|g" -i /etc/snapper/configs/home    # do not keep weekly backup
-# # sed  "s|TIMELINE_LIMIT_MONTHLY=\"10\"|TIMELINE_LIMIT_MONTHLY=\"24\"|g" -i /etc/snapper/configs/home # keep daily backup for 2 years (24)
-# # sed  "s|TIMELINE_LIMIT_YEARLY=\"10\"|TIMELINE_LIMIT_YEARLY=\"10\"|g" -i /etc/snapper/configs/home   # keep yearly backup for 10 years
+# change snap config for home directory
+sed  "s|TIMELINE_MIN_AGE=\"1800\"|TIMELINE_MIN_AGE=\"172800\"|g" -i /etc/snapper/configs/home      # keep all backup for 2 days (172800 seconds)
+sed  "s|TIMELINE_LIMIT_HOURLY=\"10\"|TIMELINE_LIMIT_HOURLY=\"96\"|g" -i /etc/snapper/configs/home  # keep daily backup fro 14 days (336 backup)
+sed  "s|TIMELINE_LIMIT_DAILY=\"10\"|TIMELINE_LIMIT_DAILY=\"14\"|g" -i /etc/snapper/configs/home    # keep daily backup for 14 days
+sed  "s|TIMELINE_LIMIT_WEEKLY=\"0\"|TIMELINE_LIMIT_WEEKLY=\"0\"|g" -i /etc/snapper/configs/home    # do not keep weekly backup
+sed  "s|TIMELINE_LIMIT_MONTHLY=\"10\"|TIMELINE_LIMIT_MONTHLY=\"24\"|g" -i /etc/snapper/configs/home # keep daily backup for 2 years (24)
+sed  "s|TIMELINE_LIMIT_YEARLY=\"10\"|TIMELINE_LIMIT_YEARLY=\"10\"|g" -i /etc/snapper/configs/home   # keep yearly backup for 10 years
 
-# setfacl -Rm "u:megavolts:rw" /etc/snapper/configs
-# setfacl -Rdm "u:megavolts:rw" /etc/snapper/configs
+setfacl -Rm "u:megavolts:rw" /etc/snapper/configs
+setfacl -Rdm "u:megavolts:rw" /etc/snapper/configs
 
-# systemctl enable snapper-boot.timer
+systemctl enable snapper-boot.timer
 
 # # Snapper before and after update
-# yaourtpkg snap-pac rsync
+yaourtpkg snap-pac rsync
 
 # # Copy partition on kernel update to enable backup
-# echo /usr/share/libalpm/hooks/50_bootbackup.hook << EOF  
-# [Trigger]
-# Operation = Upgrade
-# Operation = Install
-# Operation = Remove
-# Type = Package 
-# Target = linux* 
-# [Action] 
-# Depends = rsync 
-# Description = Backing up /boot... 
-# When = PreTransaction 
-# Exec = /usr/bin/rsync -a --delete /boot /.bootbackup
-# EOF
+echo /usr/share/libalpm/hooks/50_bootbackup.hook << EOF  
+[Trigger]
+Operation = Upgrade
+Operation = Install
+Operation = Remove
+Type = Package 
+Target = linux* 
+[Action] 
+Depends = rsync 
+Description = Backing up /boot... 
+When = PreTransaction 
+Exec = /usr/bin/rsync -a --delete /boot /.bootbackup
+EOF
 
 
 exit
